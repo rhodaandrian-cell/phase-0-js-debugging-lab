271fa67ae5ba01fdafb939b0593196a43b5ef5bf
Your Full Name <rhodaandrian@gmail.com>
2026-01-17 11:37:52 +0300

    Fixed calculateDiscountedPrice function and used debugger to identify issue

--- discountCalculator.js
+++ discountCalculator.js
@@ -1,7 +1,8 @@
 function calculateDiscountedPrice(quantity, pricePerItem) {
     let totalPrice = 0;
-    debugger
-    for (let i = 1; i < quantity; i++) {
+    debugger; // pause here to check totalPrice (it will be 0 now)
+
+    for (let i = 0; i < quantity; i++) { 
         totalPrice += pricePerItem;
     }
 
@@ -12,6 +13,8 @@ function calculateDiscountedPrice(quantity, pricePerItem) {
     return totalPrice;
 }
 
-
+// Test calls
+console.log(calculateDiscountedPrice(5, 100));
+console.log(calculateDiscountedPrice(10, 100));
 
 module.exports = calculateDiscountedPrice;
